61980fc90fe9570d9e494e65f80bc11de2e383dd
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-05 01:22:09 +0500

    Add files via upload

---
+++ HomeWork0509.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = Console.ReadLine();
+            string text = @"(^[;:])(-*)((\)))|((\())|((\[))|((\]))\S";
+            if (Regex.Match(regex, text).Success)
+            {
+                Console.WriteLine(regex + " - СМАЙЛИК");
+            }
+            else
+            {
+                Console.WriteLine(regex + " - НЕ ЯВЛЯЕТСЯ СМАЙЛИКОМ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
